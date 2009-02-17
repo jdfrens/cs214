c605566b275ceff093760a7e4764b608e1193ff8
Jeremy D. Frens <jdfrens@gmail.com>
2009-02-17 14:26:00 -0500

    better looking Mandelbrot in Haskell, but not good enough; code sucks, too

--- Haskell/Mandelbrot/ppmandel.hs
+++ Haskell/Mandelbrot/ppmandel.hs
@@ -4,6 +4,7 @@
   Creative Commons Attribution-Share Alike 3.0 United States License
 -}
 
+import List
 import System
 
 mandelWidth :: Integer
@@ -11,7 +12,9 @@ mandelWidth = 512
 mandelHeight :: Integer
 mandelHeight = 384
 
+max_size :: Float
 max_size = 4.0
+max_iters :: Integer
 max_iters = 512
 
 main :: IO ()
@@ -35,25 +38,31 @@ ppmMandelbrot x0 y0 x1 y1 =
     delta_y = (y1 - y0) / (fromInteger mandelHeight - 1.0)
     ys = coords y0 delta_y mandelHeight
     xs = coords x0 delta_x mandelWidth
-    points = [ (x,y) | x <- xs, y <- ys ]
+    points = [ (x,y) | y <- reverse ys, x <- xs ]
   in
     map plotPoint points
 
-coords :: Float -> Float -> Integer -> [Float]
-coords z delta_z n =
-  let
-    deltas = [fromInteger i * delta_z | i <- [1..n]]
-  in
-    [ z + delta | delta <- deltas]
+sorter (x0,y0) (x1,y1)
+  | x0 == x1 && y0 == y1  = EQ
+  | y0 > y1 = LT
+  | y1 < y0 = GT
+  | y0 == y1 && x0 < x1 = LT
+  | y0 == y1 && x0 > x1 = GT
+  | otherwise = EQ
 
-plotPoint p = plotPoint' p (0.0, 0.0) (0.0, 0.0) max_iters
-plotPoint'   _     _      _         0  = "0 0 0"
-plotPoint' (x,y) (a,b) (aa, bb) iters
-  | aa + bb >= max_size   = "15 15 15"
-  | otherwise             = 
-    let
-      aa' = a * a
-      bb' = b * b
-      b' = 2 * b * a + y
-      a' = aa - bb + x
-    in plotPoint' (x,y) (a',b') (aa',bb') (iters+1)
+coords :: Float -> Float -> Integer -> [Float]
+coords z delta_z n = [ z + delta | delta <- deltas ]
+  where deltas = [fromInteger i * delta_z | i <- [1..n]]
+
+plotPoint (x,y) = plotPoint' 0.0 0.0 0.0 0.0 max_iters
+  where
+    plotPoint' _ _ _  _      0  = "0 0 0"
+    plotPoint' a b aa bb iters
+      | aa + bb >= max_size   = "15 15 15"
+      | otherwise             = 
+        let
+          aa' = a * a
+          bb' = b * b
+          b' = 2 * b * a + y
+          a' = aa - bb + x
+        in plotPoint' a' b' aa' bb' (iters-1)
