0514dce7c5cbd13a417766894a86cbdff6548a1d
Jeremy D. Frens <jdfrens@gmail.com>
2009-05-06 09:41:09 -0400

    updated JavaScript code; added js execution to run task

--- Komplex/komplex.js
+++ Komplex/komplex.js
@@ -1,11 +1,41 @@
-function Komplex(real, imag) {
+function KomplexRI(real, imag) {
   this.real = real;
   this.imag = imag;
 }
 
-Komplex.prototype = {
+KomplexRI.prototype = {
   magnitude:
     function() {
       return Math.sqrt(this.real * this.real + this.imag * this.imag);
+    },
+  toString:
+    function() {
+      return this.real + "+" + this.imag + "i";
     }
 }
+
+function KomplexPR(phi, r) {
+  this.phi = phi;
+  this.r = r;
+}
+
+KomplexPR.prototype = {
+  magnitude:
+    function() {
+      return this.r;
+    },
+  toString:
+    function() {
+      return this.r + " cis " + this.phi;
+    }
+}
+
+function reportMagnitude(komplex) {
+  print("|" + komplex.toString() + "| = " + komplex.magnitude());
+}
+
+reportMagnitude(new KomplexRI(0.0, 0.0));
+reportMagnitude(new KomplexRI(5.0, 2.0));
+
+reportMagnitude(new KomplexPR(0.0, 0.0));
+reportMagnitude(new KomplexPR(0.75 * 3.14159, 1.5));
